e635e37751baf69ad077e9e95ef88ac0c8dfcb90
kbbenton509 <kevinbenton46@yahoo.com>
2018-02-15 23:35:08 -0500

    java assignment

---
+++ everything-you-need-know/java/assignments/s.java
@@ -0,0 +1,16 @@
+public class s
+{
+	public static void main(String args[]) 
+	{
+		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		
+		System.out.println("What is your name? ");
+		String name = input.nextLine();
+		
+		System.out.println("What is your age %s", name)
+		int age = reader.nextInt();
+		
+		System.out.println("What is your salary %s", name)
+		double salary = reader.nextDouble();
+	}
+}
